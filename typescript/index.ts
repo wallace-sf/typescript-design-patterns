195d81962fb47beabfbf3ae4a385619eb1402be3
wallace-sf <wallaceedua@gmail.com>
2024-03-11 00:24:24 -0300

    srp intro

---
+++ typescript/index.ts
@@ -0,0 +1,67 @@
+// The class to be refactored
+
+// class BlogPost {
+//   title: string;
+//   content: string;
+
+//   constructor(title: string, content: string) {
+//     this.title = title;
+//     this.content = content;
+//   }
+
+//   // Methods related to content management
+//   createPost() {
+//     // Implementation here
+//   }
+
+//   updatePost() {
+//     // Implementation here
+//   }
+
+//   deletePost() {
+//     // Implementation here
+//   }
+
+//   // Method related to post display
+//   displayHTML() {
+//     return `<h1>${this.title}</h1><p>${this.content}</p>`;
+//   }
+// }
+
+// Refactor the class to single responsibility principle
+
+class BlogPost {
+  title: string;
+  content: string;
+
+  constructor(title: string, content: string) {
+    this.title = title;
+    this.content = content;
+  }
+
+  // Methods related to content management
+  createPost() {
+    // Implementation here
+  }
+
+  updatePost() {
+    // Implementation here
+  }
+
+  deletePost() {
+    // Implementation here
+  }
+}
+
+class BlogPostDisplay {
+  public blogPost: BlogPost;
+
+  construct(blogPost: BlogPost) {
+    this.blogPost = blogPost;
+  }
+
+  // Method related to post display
+  displayHTML() {
+    return `<h1>${this.blogPost.title}</h1><p>${this.blogPost.content}</p>`;
+  }
+}
